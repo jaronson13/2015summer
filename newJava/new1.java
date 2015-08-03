0da72b314730523fafe2f1a648dfc71d02047765
Jesse Aronson <aronson_jesse@wheatoncollege.edu>
2015-08-03 15:05:38 -0400

    running tests with syntax

--- newJava/new1.java
+++ newJava/new1.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 class newClass {
 	public static void main(String args[]) 
 	{
-		System.out.println("cmd test");
+		Scanner myScanner = new Scanner(System.in);
+		double fnum, snum, answer;
+		System.out.println("Enter 1st number: ");
+		fnum = myScanner.nextDouble();
 		
+		System.out.println("Enter 2md number: ");
+		snum = myScanner.nextDouble();
+		
+		answer = fnum + snum;
+		
+		System.out.println(answer);
+		
+		for (int i = 0; i < 10; i++)
+		{
+			System.out.print(i);
+		}
 	}
 }
